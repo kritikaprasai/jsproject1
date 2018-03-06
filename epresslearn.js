7899d5700e4293f0c0011f08a8dabfc3bb9ed6ca
kritikaprsai <kritikaprasai397@gmail.com>
2018-03-06 17:11:11 +0545

    reinitialized files

---
+++ epresslearn.js
@@ -0,0 +1,26 @@
+var express =require ('express');
+var bodyParser =require('body-parser');
+var app = express();
+
+var urlencodedParser= bodyParser.urlencoded({ extended: false});
+
+app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+app.get('/contact', function(req,res){
+	res.render('contact', {qs: req.query});
+});
+	app.post('/contact', urlencodedParser, function(req,res){
+		console.log(req.body);
+	res.render('contact-success', {data: req.body});
+});
+
+app.get('/profile/:name',function(req,res){
+	var data = {items:'groceries', taxrate:15, fooditems:['junkfood', 'fruits','dairyproducts'] };
+res.render('profile', {person:req.params.name, data: data});
+});
+
+app.listen(3000);
---
+++ views/marketing.ejs.js
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
+	<style>
+		body{background:skyblue; font-family:verdana; color:black; padding:30px;}
+		h1{font-size:48px; text-transform:uppercase; letter-spacing: 2px; text-align:center;}
+		p{font-size:16px; text-align:center;}
+	</style>
+</head>
+<body>
+	<h1>Expenditure details of MR/MRs<%= person %></h1>
+	<p><strong>items: </strong><%= data.items %></p>
+	<p><strong>taxrate: </strong><%= data.taxrate %></p>
+<h1>Big Mart Store</h1>
+<form id="form1">
+<div ng-app="">
+<table class="auto-style1">
+<tr>
+<td class="auto-style2">first name</td>
+<td>
+<input type="text" ng-model="firstname"/>
+</td>
+</tr>
+<tr>
+<td class="auto-style2">Lastname</td>
+<td>
+<input id="text2" type="text" ng-model="Lastname"/>
+</td>
+</tr>
+<tr>
+<td class="auto-style2">Quantity</td>
+<td>
+<input  id="text3" type="text" ng-model="Quantity" />
+</td>
+</tr>
+<tr>
+<td class="auto-style2">cost</td>
+<td>
+<input id="text4" type="text" ng-model="cost" value="50">
+</td>
+</tr>
+<tr>
+<td class="auto-style2">
+<input type="button" value="clickhere"
+ng-click="message = firstname+ ' ' + Lastname +' has to pay ' + cost*Quantity" />
+</td>
+<td>
+{{message}}
+</td>
+</tr>
+</table>
+</div>
+</body>
+</html>
